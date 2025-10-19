b227abd21aedea7018930e3ca804de5bc44c38e7
Maroof Ahmed <maroofahmed@iut-dhaka.edu>
2025-10-19 11:46:52 +0600

    Ready to deploy

--- main.py
+++ main.py
@@ -379,4 +379,6 @@ async def get_genres():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    import os
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
